1055d7b756cdf47bbaee49a2ab4971d3e7684784
Bete.mrt <bete.mrt@gmail.com>
2023-03-07 01:47:44 -0300

    10º requisito

---
+++ src/models/BlogPost.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+    title: { type: DataTypes.STRING, allowNull: false },
+    content: { type: DataTypes.STRING, allowNull: false },
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    published: { type: DataTypes.DATE, allowNull: false },
+    updated: { type: DataTypes.DATE, allowNull: false },
+  },
+  {
+    timestamps: true,
+    tableName: 'blog_posts',
+    underscored: true
+  });
+
+  BlogPost.associate = models => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: {
+        name: 'userId',
+        allowNull: false
+      },
+      onDelete: 'CASCADE'
+    });
+  };
+
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -14,5 +14,9 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'postUsers' });
+  };
+
   return User;
 };
